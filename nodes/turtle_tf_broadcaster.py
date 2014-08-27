abf507277b9ea1b1e5ff5f6fee46409b7959f1b9
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2014-08-27 19:02:24 +0900

    use leader_tf param

--- nodes/turtle_tf_broadcaster.py
+++ nodes/turtle_tf_broadcaster.py
@@ -5,19 +5,19 @@ import rospy
 import tf
 import turtlesim.msg
 
-def handle_turtle_pose(msg, turtlename):
+def handle_turtle_pose(msg, turtle_name):
     br = tf.TransformBroadcaster()
     br.sendTransform((msg.x, msg.y, 0),
                      tf.transformations.quaternion_from_euler(0, 0, msg.theta),
                      rospy.Time.now(),
-                     turtlename,
+                     turtle_name,
                      'world')
 
 if __name__ == '__main__':
     rospy.init_node('turtle_tf_broadcaster')
-    turtlename = rospy.get_param('~turtle')
-    rospy.Subscriber('/%s/pose' % turtlename,
+    turtle_name = rospy.get_param('~turtle')
+    rospy.Subscriber('/%s/pose' % turtle_name,
                      turtlesim.msg.Pose,
                      handle_turtle_pose,
-                     turtlename)
+                     turtle_name)
     rospy.spin()
--- nodes/turtle_tf_listener.py
+++ nodes/turtle_tf_listener.py
@@ -9,6 +9,7 @@ import turtlesim.srv
 
 if __name__ == '__main__':
     rospy.init_node('turtle_tf_listener')
+    leader_tf = rospy.get_param('~leader_tf')
     listener = tf.TransformListener()
     rospy.wait_for_service('spawn')
     spawner = rospy.ServiceProxy('spawn', turtlesim.srv.Spawn)
@@ -17,7 +18,7 @@ if __name__ == '__main__':
     rate = rospy.Rate(10.0)
     while not rospy.is_shutdown():
         try:
-            (trans, rot) = listener.lookupTransform('/turtle2', '/turtle1', rospy.Time(0))
+            (trans, rot) = listener.lookupTransform('/turtle2', leader_tf, rospy.Time(0))
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             continue
 
